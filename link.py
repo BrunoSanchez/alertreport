04552ee6938f3fb1477013faf064b7b3a4d4cc8e
brunosanchez <bruno@oac.unc.edu.ar>
2015-11-26 18:20:34 -0300

    Changes in link for linked lists

--- link.py
+++ link.py
@@ -1,53 +1,58 @@
 
 """ Utils for linked lists in a OOP.
-You might wonder why printList and printBackward are functions and not methods in the Node class. The reason is that we want to use None to represent the empty list and it is not legal to invoke a method on None. This limitation makes it awkward to write list-manipulating code in a clean object-oriented style."""
+You might wonder why printList and printBackward are functions and not methods
+in the Node class.
+The reason is that we want to use None to represent the empty list and it is
+not legal to invoke a method on None.
+This limitation makes it awkward to write list-manipulating
+code in a clean object-oriented style."""
 
 
 class Node:
-    def __init__(self, cargo=None, next=None): 
-        self.cargo = cargo 
-        self.next  = next 
+    def __init__(self, cargo=None, next=None):
+        self.cargo = cargo
+        self.next = next
 
-    def __str__(self): 
+    def __str__(self):
         return str(self.cargo)
-    
-    def printBackward(self): 
-        if self.next != None: 
-            tail = self.next 
-            tail.printBackward() 
-        print self.cargo, 
+
+    def printBackward(self):
+        if self.next is not None:
+            tail = self.next
+            tail.printBackward()
+        print self.cargo,
 
 
-
-def printList(node): 
-    while node: 
-        print node, 
-        node = node.next 
-    print 
-
-
-def printBackward(list): 
-    if list == None: return 
-    head = list 
-    tail = list.next 
-    printBackward(tail) 
-    print head, 
+def printList(node):
+    while node:
+        print node,
+        node = node.next
+    print
 
 
-class LinkedList: 
-    def __init__(self): 
-        self.length = 0 
-        self.head   = None 
+def printBackward(list):
+    if list is None:
+        return
+    head = list
+    tail = list.next
+    printBackward(tail)
+    print head,
 
-    def printBackward(self): 
-        print "[", 
-        if self.head != None: 
-          self.head.printBackward() 
-        print "]", 
 
-    def addFirst(self, cargo): 
-        node = Node(cargo) 
-        node.next = self.head 
-        self.head = node 
-        self.length = self.length + 1 
+class LinkedList:
+    def __init__(self):
+        self.length = 0
+        self.head = None
+
+    def printBackward(self):
+        print "[",
+        if self.head is not None:
+            self.head.printBackward()
+        print "]",
+
+    def addFirst(self, cargo):
+        node = Node(cargo)
+        node.next = self.head
+        self.head = node
+        self.length = self.length + 1
 
